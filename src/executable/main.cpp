801c4a61b39d6f1b4badf578ca2ed99f3d84e312
Denis Bukhalov <buhalovdenisomsk@gmail.com>
2024-08-09 18:08:00 +0300

    [*] created CMakeFiles.txt for each target [+] added private/public folders for library

--- src/executable/main.cpp
+++ src/executable/main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 
+// from header_only library
 #include "hello_header_only.hpp"
+
+// from public library's headers
 #include "hello_library.hpp"
+
+// UNCOMMENTING SHOULD LEAD TO COMPILE ERROR
+// from private library's headers
+// #include "private_header.hpp"
 
 int main() {
 
   std::cout << "Hello, from executable!\n";
   hello_from_header_only();
   hello_library();
+
+  // this functions should be not visible here
+  // private_function();
 
   return 0;
 }
